927a2bd582e817f0941dc7bd07444e7a6919023e
GuiOliveira1104 <133981211+GuiOliveira1104@users.noreply.github.com>
2026-01-16 19:35:55 -0300

    Update teste_trello.py
    apagando credenciais

--- teste_trello.py
+++ teste_trello.py
@@ -3,8 +3,8 @@ import json
 
 # retorna as pranchas (boards)
 
-apiKey = '13a45376e8770f8efe9450e0974de27d'
-apiToken = 'ATTAef2ff1e1acbc3e69e1128b6cc6179afec94c7a2c5d49abb165a277602f335574CFF83B61'
+apiKey = ''
+apiToken = ''
 
 url = f"https://api.trello.com/1/members/me/boards?fields=name,url&key={apiKey}&token={apiToken}"
 
@@ -19,3 +19,4 @@ response = requests.request(
 )
 
 print(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")))
+
